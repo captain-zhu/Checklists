412d9f67ecb813bf609c74cc215b209713937648
captain-zhu <shibaier@foxmail.com>
2015-08-03 16:08:09 +0800

    在新界面添加了textfiled，用于添加新的to-dolist项目

---
+++ Checklists/AddItemViewController.h
@@ -0,0 +1,19 @@
+//
+//  AddItemViewController.h
+//  Checklists
+//
+//  Created by zhu yongxuan on 15/8/3.
+//  Copyright (c) 2015年 zhu yongxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddItemViewController : UITableViewController<UITextViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *doneBarButton;
+
+- (IBAction)cancel;
+- (IBAction)done;
+
+@end
---
+++ Checklists/AddItemViewController.m
@@ -0,0 +1,109 @@
+//
+//  AddItemViewController.m
+//  Checklists
+//
+//  Created by zhu yongxuan on 15/8/3.
+//  Copyright (c) 2015年 zhu yongxuan. All rights reserved.
+//
+
+#import "AddItemViewController.h"
+
+@interface AddItemViewController ()
+
+@end
+
+@implementation AddItemViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self.textField becomeFirstResponder];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (IBAction)cancel
+{
+    [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)done
+{
+    NSLog(@"Contents of the text field: %@", self.textField.text);
+    
+    [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
+
+- (BOOL)textField:(UITextField *)textField
+        shouldChangeCharactersInRange:(NSRange)range
+        replacementString:(NSString *)string
+{
+    NSString *newText = [textField.text stringByReplacingCharactersInRange:range
+                                withString:string];
+    
+    self.doneBarButton.enabled = ([newText length] > 0);
+    
+    return YES;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@end
