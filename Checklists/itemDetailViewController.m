1a70a1f57e9c84d01b5abf4de6b2da066edb4ccc
captain-zhu <shibaier@foxmail.com>
2015-08-10 16:39:55 +0800

    添加了datepicker，可以选定目标日期

--- Checklists/itemDetailViewController.m
+++ Checklists/itemDetailViewController.m
@@ -15,6 +15,7 @@
 @implementation ItemDetailViewController
 {
     NSDate *_dueDate;
+    BOOL _datePickVisible;
 }
 
 - (id)initWithStyle:(UITableViewStyle)style
@@ -53,6 +54,44 @@
     self.dueDateLabel.text = [formatter stringFromDate:_dueDate];
 }
 
+- (void)showDatePicker
+{
+    _datePickVisible = YES;
+    
+    NSIndexPath *indexPathDateRow = [NSIndexPath indexPathForRow:1 inSection:1];
+    NSIndexPath *indexPathDatePicker = [NSIndexPath indexPathForRow:2 inSection:1];
+    
+    UITableViewCell *cell =[self.tableView cellForRowAtIndexPath:indexPathDateRow];
+    cell.detailTextLabel.textColor = cell.detailTextLabel.tintColor;
+    
+    [self.tableView beginUpdates];
+    [self.tableView insertRowsAtIndexPaths:@[indexPathDatePicker] withRowAnimation:UITableViewRowAnimationFade];
+    [self.tableView reloadRowsAtIndexPaths:@[indexPathDateRow] withRowAnimation:UITableViewRowAnimationNone];
+    [self.tableView endUpdates];
+    
+    UITableViewCell *datePickerCell = [self.tableView cellForRowAtIndexPath:indexPathDatePicker];
+    UIDatePicker *datePick = (UIDatePicker *)[datePickerCell viewWithTag:100];
+    [datePick setDate:_dueDate animated:NO];
+}
+
+- (void)hideDatePicker
+{
+    if (_datePickVisible) {
+        _datePickVisible = NO;
+        
+        NSIndexPath *indexPathDateRow = [NSIndexPath indexPathForRow:1 inSection:1];
+        NSIndexPath *indexPathDatePicker = [NSIndexPath indexPathForRow:2 inSection:1];
+        
+        UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPathDateRow];
+        cell.detailTextLabel.textColor = [UIColor colorWithWhite:0.0f alpha:0.5f];
+        
+        [self.tableView beginUpdates];
+        [self.tableView reloadRowsAtIndexPaths:@[indexPathDateRow] withRowAnimation:UITableViewRowAnimationNone];
+        [self.tableView deleteRowsAtIndexPaths:@[indexPathDatePicker] withRowAnimation:UITableViewRowAnimationFade];
+        [self.tableView endUpdates];
+    }
+}
+
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
@@ -64,6 +103,83 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    [self hideDatePicker];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    [self.textField resignFirstResponder];
+    
+    if (indexPath.row == 1 && indexPath.section == 1) {
+        if (!_datePickVisible) {
+            [self showDatePicker];
+        } else {
+            [self hideDatePicker];
+        }
+    }
+}
+
+- (void)dateChanged:(UIDatePicker *)datePicker
+{
+    _dueDate = datePicker.date;
+    [self updateDueDateLabel];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.section == 1 && indexPath.row == 2) {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"DatePickerCell"];
+        if (cell == nil) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                          reuseIdentifier:@"DatePickerCell"];
+            cell.selectionStyle = UITableViewCellSelectionStyleNone;
+            
+            UIDatePicker *datePicker =[[UIDatePicker alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 320.0f, 216.0f)];
+            
+            datePicker.tag = 100;
+            [cell.contentView addSubview:datePicker];
+            
+            [datePicker addTarget:self action:@selector(dateChanged:) forControlEvents:UIControlEventValueChanged];
+        }
+        return cell;
+    } else  {
+        return [super tableView:tableView cellForRowAtIndexPath:indexPath];
+    }
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (section == 1 && _datePickVisible) {
+        return 3;
+    } else {
+        return [super tableView:tableView numberOfRowsInSection:section];
+    }
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.section == 1 && indexPath.row == 2) {
+        return 217.0f;
+    } else {
+        return [super tableView:tableView heightForRowAtIndexPath:indexPath];
+    }
+}
+
+- (NSInteger)tableView:(UITableView *)tableView indentationLevelForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.section == 1 && indexPath.row == 2) {
+        NSIndexPath *newIndexPath = [NSIndexPath indexPathForRow:0 inSection:indexPath.section];
+        
+        return [super tableView:tableView indentationLevelForRowAtIndexPath:newIndexPath];
+    } else {
+        return [super tableView:tableView indentationLevelForRowAtIndexPath:indexPath];
+    }
 }
 
 - (IBAction)cancel
@@ -92,7 +208,12 @@
 
 - (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return nil;
+    if (indexPath.row == 1 && indexPath.section == 1) {
+        return indexPath;
+    } else {
+        return nil;
+    }
+    
 }
 
 - (BOOL)textField:(UITextField *)theTextField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
